0f7f26a8c9413e3ed05ce4cb35972ea5681d4bfa
Baibhab Swain <26348294+Baibhabswain@users.noreply.github.com>
2020-12-04 19:59:25 +0530

    Update singleLinked_list.java

--- singleLinked_list.java
+++ singleLinked_list.java
@@ -1,225 +1,200 @@
-package assin_4;
-import java.util.*;
-class node
-{
-	int info;
-	node next;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class IntSLLNode {
+	public int info;
+	public IntSLLNode next;
+
+	public IntSLLNode(int i) {
+		this(i, null);
+	}
+
+	public IntSLLNode(int i, IntSLLNode n) {
+		info = i;
+		next = n;
+	}
 }
 
-public class program11 {
+class IntSLList {
+	protected IntSLLNode head, tail;
 
-	@SuppressWarnings("resource")
-	public static node create(node start)
-	{
-		char option;
-		node p,q;
-		p = new node();
-		Scanner sc=new Scanner(System.in);
-		System.out.print("enter info");
-		p.info = sc.nextInt();
-		p.next = null;
-		start = p;
-		System.out.println("do you want to create morenodes(y/n)");
-		option = sc.next().charAt(0);
-	        q = p;
-		while (option =='y')
-		{
-			p = new node ();
-			System.out.println(" enter info");
-			p.info=sc.nextInt();
-			p.next = null;
-			q.next = p;
-			System.out.println("do you want to create morenodes(y/n)");
-			option = sc.next().charAt(0);
-		}
-  		return start;
+	public IntSLList() {
+		head = tail = null;
 	}
 
-	public static void display(node start)
-	{
-		node p;
-		if (start == null)
-		
-			System.out.println("LL is empty");
-		else 
-		{ 
-			p = start;
-			while (p != null)
-			{ 
-				System.out.print(p.info+"-->");
-				p = p.next;
-			}
-		}
+	public boolean isEmpty() {
+		return head == null;
 	}
 
-	@SuppressWarnings("resource")
-	public static node insbeg(node start)
-	{
-		node p;
-		p = new node ();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter info");
-		p.info = sc.nextInt();
-		p.next = start;
-	
-		return start = p;
+	public void addToHead(int el) {
+		head = new IntSLLNode(el, head);
+		if (tail == null)
+			tail = head;
 	}
 
-	@SuppressWarnings({ "null", "resource" })
-	public static node insend(node start)
-	{
-		node p,q;
-		p = new node();
-		q = new node();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter info");
-		p.info = sc.nextInt();
-		p.next = null;
-		q = start;
-		while(q != null)
-			q = q.next;
-		
-		q.next = p;
-		return start;
+	public void addToTail(int el) {
+		if (!isEmpty()) {
+			tail.next = new IntSLLNode(el);
+			tail = tail.next;
+		} else
+			head = tail = new IntSLLNode(el);
 	}
 
-	@SuppressWarnings({ "null", "resource" })
-	public static node insanypos(node start)
-	{
-		node p,q;
-	
-		p = new node();
-		q = new node();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter info");
-		p.info = sc.nextInt();
-		int pos;int count = 0;
-		System.out.println("enter position");
-		pos = sc.nextInt();
-		q = start;
-		while(q != null);
-		{
-			count++;
-			q = q.next;
-		}
-		if(pos >= count++)
-		{
-			System.out.println("insertion not possible");
-		}
-		
+	public boolean isInList(int el) {
+		IntSLLNode temp;
+		for (temp = head; temp != null && temp.info != el; temp = temp.next)
+			;
+		return temp != null;
+	}
+
+	public void printAll() {
+		for (IntSLLNode temp = head; temp != null; temp = temp.next)
+			System.out.print(temp.info + " ");
+		System.out.println();
+	}
+
+	public int deleteFromHead() {
+		int el = head.info;
+		if (head == tail)
+			head = tail = null;
 		else
-		{
-			if (pos == 1)
-			{
-				
-				p.next = start;
-				return start=p;
-			}
-			
-			else if(pos == count++)
-			{
-				p.next = null;
-				while(q != null)
-					q = q.next;
-					
-			}
-			
-			else
-			{
-				node p1,p2;
-				int i = 0;
-				p1 = start;
-				while(i<pos)
-				{
-					p2 = p1;
-					p1 = p.next;
-					i++;
-					p.next = p1;
-					p2.next = p;
-							
-				}
-			}
-		}
-		return start;
-	}
-				
-	public static node deletebeg(node start)
-	{
-		node p = start;
-		if(start == null)
-			System.out.println("ll is empty");
-						
-		else
-		{
-			start = start.next;
-			p.next = null;
-			System.out.println("the deleted node is p.info");
-		}
-		return start;
+			head = head.next;
+		return el;
 	}
 
-	public static node delend(node start)
-	{
-		node p = start;
-		if(start==null)
-			System.out.println("the deleted node is p.info");
-				
-		else
-		{
-			while(p.next.next!=null)
-				p = p.next;
-			
-			System.out.println("the deleted node is p.next.info");
-			p.next = null;
+	public int deleteFromTail() {
+		int el = tail.info;
+		if (head == tail)
+			head = tail = null;
+		else {
+			IntSLLNode temp;
+			for (temp = head; temp.next != tail; temp = temp.next)
+				;
+			tail = temp;
+			tail.next = null;
 		}
-		return start;
+		return el;
 	}
 
-	public static void main(String[] args) 
-	{
-		Scanner sc=new Scanner(System.in);
+	public boolean delete(int el) {
+		if (head == tail && el == head.info) {
+			head = tail = null;
+			return true;
+		} else if (el == head.info) {
+			head = head.next;
+			return true;
+		} else {
+			IntSLLNode pred, temp;
+			for (pred = head, temp = head.next; temp != null && temp.info != el; pred = pred.next, temp = temp.next)
+				;
+			if (temp != null) {
+				pred.next = temp.next;
+				if (temp == tail)
+					tail = pred;
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+public class SinglyLinkedList {
+	public static void main(String args[]) throws IOException {
+		IntSLList list = new IntSLList();
+
 		int choice;
-		node start = null;
-		while(true)
-		{
-			System.out.println("****MENU*****");
-			System.out.println("0:Exit");
-			System.out.println("1:Creation");
-			System.out.println("2:Display");
-			System.out.println("3:Insert at beginnig");
-			System.out.println("4:Insert at end");
-			System.out.println("5:Insert from any position");
-			System.out.println("6:Delete at beg");
-			System.out.println("7:Delete at end");
-			System.out.println("Enter your choice");
-			choice=sc.nextInt();
-			switch(choice)
-			{
-				case 0:
-				System.exit(0);
+		int el;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		do {
+			System.out.println("Enter your choice:");
+			System.out
+					.println("1.ADD \t2.DELETE \t3.PRINT ALL \t4.SEARCH \t5.EXIT");
+			choice = Integer.parseInt(br.readLine());
+			switch (choice) {
+			case 1:
+				System.out.println("\t\tEnter your choice:");
+				System.out.println("\t\t1.ADD TO HEAD \t2.ADD TO TAIL");
+				System.out.print("\t\t");
+				choice = Integer.parseInt(br.readLine());
+				switch (choice) {
 				case 1:
-				start = create(start);
-				break;
+					System.out.println("\t\tEnter an element");
+					System.out.print("\t\t");
+					el = Integer.parseInt(br.readLine());
+					list.addToHead(el);
+					break;
 				case 2:
-				display(start);
-				break;
-				case 3:
-				start = insbeg(start);
-				break;
-				case 4:
-				start = insend(start);
-				break;
-				case 5:
-				start = insanypos(start);
-				break;
-					case 6:
-				start = deletebeg(start);
-				break;
-				case 7:
-				start = delend(start);
+					System.out.println("\t\tEnter an element");
+					System.out.print("\t\t");
+					el = Integer.parseInt(br.readLine());
+					list.addToTail(el);
+					break;
 				default:
-				System.out.println("Wrong choice");
+					System.out
+							.println("\t\tInvalid argument,return to main menu");
+					break;
+				}
+				break;
+			case 2:
+				if (list.isEmpty())
+					System.out.println("\t\tList is empty");
+				else {
+					System.out.println("\t\tEnter your choice:");
+					System.out
+							.println("\t\t1.DELETE FROM HEAD \t2.DELETE FROM TAIL \t3.DELETE A PARTICULAR ELEMENT");
+					System.out.print("\t\t");
+					choice = Integer.parseInt(br.readLine());
+					switch (choice) {
+					case 1:
+						el = list.deleteFromHead();
+						System.out.println("\t\tElement deleted is:" + el);
+						break;
+					case 2:
+						el = list.deleteFromTail();
+						System.out.println("\t\tElement deleted is:" + el);
+						break;
+					case 3:
+						System.out.println("\t\tEnter an element");
+						System.out.print("\t\t");
+						el = Integer.parseInt(br.readLine());
+						if (list.delete(el))
+							System.out.println("\t\tElement is deleted");
+						else
+							System.out.println("\t\tNot found");
+						break;
+					default:
+						System.out
+								.println("\t\tInvalid argument,return to main menu");
+						break;
+					}
+				}
+				break;
+			case 3:
+				if (list.isEmpty())
+					System.out.println("\t\tList is empty");
+				else {
+					System.out.println("\t\tElements in the list are:");
+					System.out.print("\t\t");
+					list.printAll();
+				}
+				break;
+			case 4:
+				if (list.isEmpty())
+					System.out.println("\t\tList is empty");
+				else {
+					System.out.println("\t\tEnter your choice:");
+					System.out.print("\t\t");
+					el = Integer.parseInt(br.readLine());
+					if (list.isInList(el))
+						System.out.println("\t\tElement is present");
+					else
+						System.out.println("\t\tElement is not present");
+				}
+				break;
+			case 5:
+				break;
 			}
-		}
+		} while (choice != 5);
 	}
-}
